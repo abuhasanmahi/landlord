c0615098bfb167725c538a493ecd4d5259b91e21
Abu Hasan Mahi <abu.hasan.mahi@gmail.com>
2022-09-13 23:34:11 +0600

    adminlte incorporated

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
+    <title>{{ config('app.name', 'Landlord') }}</title>
+    
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="{{asset('/dashboard/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('/dashboard/dist/css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            </ul>
+            <div class="mx-auto order-0">
+                <!--<img src="dashboard/dist/img/AdminLTELogo.png">-->
+            </div>
+        </nav>
+        <!-- /.navbar -->
+        
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="#" class="brand-link">
+            <img src="dashboard/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">Landlord</span>
+            </a>
+        
+            <!-- Sidebar -->
+            <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                <img src="dashboard/dist/img/noimage.jpg" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                <a href="#" class="d-block"></a>
+                </div>
+            </div>
+        
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas fa-home"></i>
+                            <p>
+                                Home
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="tenant/index.php" class="nav-link">
+                            <i class="nav-icon fas fa-user"></i>
+                            <p>
+                                Tenant Info
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="flat/index.php" class="nav-link">
+                            <i class="nav-icon fas fa-building"></i>
+                            <p>
+                                Flat Info
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="generate/index.php" class="nav-link">
+                            <i class="nav-icon fas fa-receipt"></i>
+                            <p>
+                                Generate Receipt
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-home"></i>
+                            <p>
+                                Logout
+                            </p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+        
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark"></h1>
+                </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+        
+            <!-- Main content -->
+            <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10">
+                        @yield('content')
+                    </div>
+                    <div class="col-md-1"></div>
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+            <h5>Title</h5>
+            <p>Sidebar content</p>
+            </div>
+        </aside>
+        <!-- /.control-sidebar -->
+        
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+            Version 1.0
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright &copy; {{date('Y')}}
+            <a href="#">Landlord</a>.</strong> All rights reserved.
+        </footer>
+    </div>
+<!-- ./wrapper -->
+ 
+<!-- REQUIRED SCRIPTS -->
+ 
+<!-- jQuery -->
+<script src="{{asset('/dashboard/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('/dashboard/dist/js/adminlte.min.js')}}"></script>
+</body>
+</html>
