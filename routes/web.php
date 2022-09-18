6be698ffa3fb0457af8b984f74b236b6dc2cf5f5
Abu Hasan Mahi <abu.hasan.mahi@gmail.com>
2022-09-18 11:14:51 +0600

    middleware auth added to route

--- routes/web.php
+++ routes/web.php
@@ -31,23 +31,25 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::get('/logout', [LogoutController::class, 'index'])->name('logout');
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/home', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/home', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/flat', [FlatController::class, 'index'])->name('flat');
-Route::get('/flat/create', [FlatController::class, 'create'])->name('flat.create');
-Route::post('/flat', [FlatController::class, 'store']);
-Route::get('/flat/{id}/edit', [FlatController::class, 'edit'])->name('flat.edit');
-Route::post('/flat/{id}/update', [FlatController::class, 'update'])->name('flat.update');
-Route::get('/flat/{id}/destroy', [FlatController::class, 'destroy']);
+    Route::get('/flat', [FlatController::class, 'index'])->name('flat');
+    Route::get('/flat/create', [FlatController::class, 'create'])->name('flat.create');
+    Route::post('/flat', [FlatController::class, 'store']);
+    Route::get('/flat/{id}/edit', [FlatController::class, 'edit'])->name('flat.edit');
+    Route::post('/flat/{id}/update', [FlatController::class, 'update'])->name('flat.update');
+    Route::get('/flat/{id}/destroy', [FlatController::class, 'destroy']);
 
-Route::get('/tenant', [TenantController::class, 'index'])->name('tenant');
-Route::get('/tenant/create', [TenantController::class, 'create'])->name('tenant.create');
-Route::post('/tenant', [TenantController::class, 'store']);
-Route::get('/tenant/{id}/edit', [TenantController::class, 'edit'])->name('tenant.edit');
-Route::post('/tenant/{id}/update', [TenantController::class, 'update'])->name('tenant.update');
-Route::get('/tenant/{id}/destroy', [TenantController::class, 'destroy']);
+    Route::get('/tenant', [TenantController::class, 'index'])->name('tenant');
+    Route::get('/tenant/create', [TenantController::class, 'create'])->name('tenant.create');
+    Route::post('/tenant', [TenantController::class, 'store']);
+    Route::get('/tenant/{id}/edit', [TenantController::class, 'edit'])->name('tenant.edit');
+    Route::post('/tenant/{id}/update', [TenantController::class, 'update'])->name('tenant.update');
+    Route::get('/tenant/{id}/destroy', [TenantController::class, 'destroy']);
 
-Route::get('/receipt', [ReceiptController::class, 'index'])->name('receipt');
-Route::post('/receipt/generate', [ReceiptController::class, 'generate'])->name('receipt.generate');
+    Route::get('/receipt', [ReceiptController::class, 'index'])->name('receipt');
+    Route::post('/receipt/generate', [ReceiptController::class, 'generate'])->name('receipt.generate');
+});
